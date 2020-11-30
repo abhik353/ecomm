029dbd7dfec38d1755382e45d6111dadcbf39c1f
Abhinav <abhinavk353.ak@gmail.com>
2020-11-30 14:33:48 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+
+const App = () => {
+
+  return (
+    <div className="App"> 
+      <Header/>
+      <main>
+        welcome 
+      </main>
+      <Footer/> 
+    </div>
+  );
+}
+
+export default App;
